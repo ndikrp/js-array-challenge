25886b822cac978e0ce571163d84a0d789228ed4
ndikrp <andhikarizky36@gmail.com>
2024-03-03 17:43:13 +0700

    fix filter car, sort ascend, and sort descend

--- scripts/sortCarByYearDecendingly.js
+++ scripts/sortCarByYearDecendingly.js
@@ -7,7 +7,17 @@ function sortCarByYearDescendingly(cars) {
   const result = [...cars];
 
   // Tulis code-mu disini
-
+  for (let i = 0; i < result.length; i++) {
+    for (let j = 0; j < result.length - 1; j++) {
+      if (result[j].year < result[j + 1].year) {
+        let temp = result[j]
+        result[j] = result[j + 1]
+        result[j + 1] = temp
+      }
+    }
+  }
+  console.log(result)
+  console.table(result)
   // Rubah code ini dengan array hasil sorting secara descending
   return result;
 }
